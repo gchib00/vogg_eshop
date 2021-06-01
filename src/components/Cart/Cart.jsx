d961e392c27e6c57d8c60f600219b0fc97d6127a
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2021-06-01 20:55:14 +0400

    improved shopping cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Grid, Typography, Button, Container } from '@material-ui/core'
+
 import useStyles from './styles'
+import CartItem from './CartItem/CartItem'
 
 
-function Cart ({cart}) {
+function Cart ({ cart }) {
 
     const classes = useStyles()
 
@@ -20,7 +22,7 @@ function Cart ({cart}) {
                 <Grid container spacing={3}>
                     {cart.line_items.map((item) => (
                         <Grid item xs={12} sm={4} key={item.id}>
-                            <div>{item.name}</div>
+                            <CartItem item={item} />
                         </Grid>
                     ))}
                 </Grid>
